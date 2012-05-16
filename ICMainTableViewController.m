4a0717370f76a626311383ed79069a3d38bc4905
Kevin Shutzberg <jkevin@stanford.edu>
2012-05-16 00:20:08 -0700

    Implemented "give us feedback" link to the iTunes store.

--- ICMainTableViewController.m
+++ ICMainTableViewController.m
@@ -49,9 +49,13 @@
     [self.navigationController pushViewController:songsTVC animated:YES];
 }
 
-- (void)showSettings
+- (void)showFeedback
 {
-    
+    NSString *appID = @"527835521";
+    NSString *itunesPath = [NSString stringWithFormat:@"http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=%@&pageNumber=0&sortOrdering=1&type=Purple+Software&mt=8",appID];
+    NSURL *itunesURL = [NSURL URLWithString:itunesPath];
+
+    [[UIApplication sharedApplication] openURL:itunesURL];
 }
 
 #pragma mark - Table view delegate
@@ -62,7 +66,7 @@
         MainRowPlaylists    = 1,
         MainRowArtists      = 2,
         MainRowSongs        = 3,
-        MainRowSettings     = 4,
+        MainRowFeedback     = 4,
     };
     
     enum MainRow row = indexPath.row;
@@ -80,8 +84,8 @@
         case MainRowSongs:
             [self showSongs];
             break;
-        case MainRowSettings:
-            [self showSettings];
+        case MainRowFeedback:
+            [self showFeedback];
             break;
     }
 }
